4618875d6c18525c7f66076f744a06a4981076e5
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-04-21 00:16:30 +0530

    Update Insertionsort.py

--- Insertionsort.py
+++ Insertionsort.py
@@ -11,3 +11,13 @@ for j in range(2,len(a)):
     a[i+1]=key
 
 print(a)
+
+
+# New improved code with values of i and j
+a=[9,6,5,2,1]
+for i in range(len(a)):                           # outer loop variable i 
+    for j in range(i+1,len(a)):                   # inner loop variable j
+        if a[i]>a[j]:                             # comparison
+            a[i],a[j]=a[j],a[i]                   # swapping
+            print(a)
+        print('i='+str(i  )+'   j='+str(j))
